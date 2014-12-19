f715e01e6417371c86665d3b852e75d2d1438011
Hok Shun Poon <hok@hokshunpoon.me>
2014-12-19 19:46:55 +0000

    Initial Commit

---
+++ Vanguard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vanguard
+//
+//  Created by Hok Shun Poon on 19/12/2014.
+//  Copyright (c) 2014 Hok Shun Poon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
