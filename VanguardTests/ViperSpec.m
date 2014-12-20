63790e7db303c59595c66ddad8586f255ac83a88
Hok Shun Poon <hok@hokshunpoon.me>
2014-12-20 01:27:50 +0000

    Rename to Viper spec.

---
+++ VanguardTests/ViperSpec.m
@@ -0,0 +1,19 @@
+//
+//  ViperSpec.m
+//  Vanguard-iOS
+//
+//  Created by Hok Shun Poon on 19/12/2014.
+//  Copyright (c) 2014 Hok Shun Poon. All rights reserved.
+//
+
+#define EXP_SHORTHAND
+
+#import <Expecta/Expecta.h>
+#import <Specta/Specta.h>
+
+SpecBegin(Viper)
+
+describe(@"Viper application", ^{
+});
+
+SpecEnd
