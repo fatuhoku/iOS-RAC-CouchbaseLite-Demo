21c81df2a73bd198656e2eb4d65f4b297c17c927
Hok Shun Poon <hok@hokshunpoon.me>
2014-12-20 12:39:33 +0000

    Add CellViewModel

---
+++ Vanguard/Assemblies/RecipeCollection/MESRecipeCollectionViewCellViewModel.h
@@ -0,0 +1,11 @@
+//
+// Created by Hok Shun Poon on 20/12/14.
+// Copyright (c) 2014 Hok Shun Poon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MESRecipeCollectionViewCellViewModel : NSObject
+@property(nonatomic, copy) NSString *recipeName;
+@end
---
+++ Vanguard/Assemblies/RecipeCollection/MESRecipeCollectionViewCellViewModel.m
@@ -0,0 +1,10 @@
+//
+// Created by Hok Shun Poon on 20/12/14.
+// Copyright (c) 2014 Hok Shun Poon. All rights reserved.
+//
+
+#import "MESRecipeCollectionViewCellViewModel.h"
+
+
+@implementation MESRecipeCollectionViewCellViewModel
+@end
