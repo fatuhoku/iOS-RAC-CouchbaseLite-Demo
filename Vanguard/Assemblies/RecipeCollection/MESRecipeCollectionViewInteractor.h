e5de7fe607c5c91ab477a984aa97efa083a753b3
Hok Shun Poon <hok@hokshunpoon.me>
2014-12-21 10:59:27 +0000

    Add interactor class.

---
+++ Vanguard/Assemblies/RecipeCollection/MESRecipeCollectionViewInteractor.h
@@ -0,0 +1,10 @@
+//
+// Created by Hok Shun Poon on 21/12/14.
+// Copyright (c) 2014 Hok Shun Poon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MESRecipeCollectionViewInteractor : NSObject
+@end
---
+++ Vanguard/Assemblies/RecipeCollection/MESRecipeCollectionViewInteractor.m
@@ -0,0 +1,11 @@
+//
+// Created by Hok Shun Poon on 21/12/14.
+// Copyright (c) 2014 Hok Shun Poon. All rights reserved.
+//
+
+#import "MESRecipeCollectionViewInteractor.h"
+
+
+@implementation MESRecipeCollectionViewInteractor
+
+@end
