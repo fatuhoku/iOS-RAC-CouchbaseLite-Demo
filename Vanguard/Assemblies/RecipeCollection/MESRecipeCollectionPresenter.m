d41721f9f3c5a91e1e3376eb2dbedcc27e2766aa
Hok Shun Poon <hok@hokshunpoon.me>
2015-01-09 17:17:39 +0000

    Push tableview data source responsibility to view model.

--- Vanguard/Assemblies/RecipeCollection/MESRecipeCollectionPresenter.m
+++ Vanguard/Assemblies/RecipeCollection/MESRecipeCollectionPresenter.m
@@ -3,23 +3,46 @@
 // Copyright (c) 2014 Hok Shun Poon. All rights reserved.
 //
 
+#import "MESRecipeCellViewModel.h"
+#import "MESSharedAssembly.h"
+#import "MESRecipeCollectionViewController.h"
 #import <Typhoon/TyphoonAutoInjection.h>
 #import <TLIndexPathTools/TLIndexPathDataModel.h>
 #import "MESRecipeCollectionPresenter.h"
 #import "MESRecipeCollectionInteractor.h"
 
 @interface MESRecipeCollectionPresenter ()
-@property(nonatomic, strong) InjectedClass(MESRecipeCollectionInteractor) interactor;
+@property(nonatomic, strong) InjectedClass(MESSharedAssembly)assembly;
+@property(nonatomic, strong) InjectedClass(MESRecipeCollectionInteractor)interactor;
 @end
 
 @implementation MESRecipeCollectionPresenter
 
 - (void)typhoonDidInject {
-    RAC(self, recipesDataModel) = [RACObserve(self, interactor.recipeEntities) map:^TLIndexPathDataModel *(NSArray *recipeEntities) {
+    self.indexPathController = [TLIndexPathController new];
+
+    RAC(self, indexPathController.dataModel) = [RACObserve(self, interactor.recipeEntities) map:^TLIndexPathDataModel *(NSArray *recipeEntities) {
         return [[TLIndexPathDataModel alloc] initWithItems:recipeEntities];
     }];
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.indexPathController.dataModel numberOfRowsInSection:section];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    id cell = [tableView dequeueReusableCellWithIdentifier:@"ruid_recipeCollectionViewCell" forIndexPath:indexPath];
+
+    id recipe = [self.indexPathController.dataModel itemAtIndexPath:indexPath];
+    [self configureCell:cell withModel:recipe];
+
+    return cell;
+}
+
+- (void)configureCell:(UITableViewCell *)cell withModel:(id)recipe {
+    MESRecipeCellViewModel *viewModel = [self.assembly recipeCollectionCellViewModelForRecipe:recipe];
+    cell.textLabel.text = viewModel.title;
+}
 @end
 
 @implementation MESRecipeCollectionPresenter (Commands)
--- Vanguard/Assemblies/RecipeCollection/MESRecipeCollectionViewController.m
+++ Vanguard/Assemblies/RecipeCollection/MESRecipeCollectionViewController.m
@@ -10,13 +10,10 @@
 #import "MESRecipeCollectionViewController.h"
 #import "MESRecipeCollectionPresenter.h"
 #import "MESSharedAssembly.h"
-#import "MESRecipeCellViewModel.h"
 
 
 @interface MESRecipeCollectionViewController () <TLIndexPathControllerDelegate>
-@property(nonatomic, strong) InjectedClass(MESSharedAssembly)assembly;
 @property(nonatomic, strong) InjectedClass(MESRecipeCollectionPresenter)presenter;
-@property(nonatomic, strong) InjectedClass(TLIndexPathController)indexPathController;
 @end
 
 @implementation MESRecipeCollectionViewController
@@ -24,16 +21,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.indexPathController.delegate = self;
-
     NSAssert(self.presenter, @"presenter is nil");
-
-    // Can always ask the Presenter for individual view models.
-    // TODO Is there a way that the Presenter can be aware of the indexPathController,
-    // & handle the delegate method AND push updates to the view?
-    // IndexPathController could
-    RAC(self, indexPathController.dataModel) = RACObserve(self, presenter.recipesDataModel);
+    self.presenter.indexPathController.delegate = self;
+    self.tableView.dataSource = self.presenter;
 }
+
 
 #pragma mark - Action
 
@@ -42,24 +34,6 @@
 }
 
 #pragma mark - Table View Delegate
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [self.indexPathController.dataModel numberOfRowsInSection:section];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    id cell = [tableView dequeueReusableCellWithIdentifier:@"ruid_recipeCollectionViewCell" forIndexPath:indexPath];
-
-    id recipe = [self.indexPathController.dataModel itemAtIndexPath:indexPath];
-    [self configureCell:cell withModel:recipe];
-
-    return cell;
-}
-
-- (void)configureCell:(UITableViewCell *)cell withModel:(id)recipe {
-    MESRecipeCellViewModel *viewModel = [self.assembly recipeCollectionCellViewModelForRecipe:recipe];
-    cell.textLabel.text = viewModel.title;
-}
 
 - (void)controller:(TLIndexPathController *)controller didUpdateDataModel:(TLIndexPathUpdates *)updates {
     [updates performBatchUpdatesOnTableView:self.tableView withRowAnimation:UITableViewRowAnimationAutomatic];
